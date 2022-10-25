2a7195a03557b015ed4b2f2cf8ec45b82af5f20c
BrianHCombes <bcombes@tarptiedown.com>
2022-10-25 10:07:31 -0600

    Have expanded (a bit) the routing needs. Now to updating state changes for proper menu highlighting.

--- lrreact/src/lr-layout-page/Layout.js
+++ lrreact/src/lr-layout-page/Layout.js
@@ -1,24 +1,79 @@
 import React from 'react';
+import { useState } from "react";
 import { Outlet, Link } from 'react-router-dom';
 
-const Layout = () => {
-  return (
-         
-    <div style={{textAlign:"center"}}> 
+import titleBanner01 from '../images/lr-title-banner_01.jpg';
+import titleBanner02 from '../images/lr-title-banner_02.jpg';
+import {lr} from '../lr/lr-single.js';
+import './Layout.css';
+
+
+
+function update(){
+    window.setTimeout(getURL,1000);
     
-          <br /><br /><br /><br /><br /><br />
+    //let URL;
+    
+    function getURL(){ 
+                                  //URL = window.location.href;
+                                  console.log("window.location.href is: " + window.location.href);
+                                 };
+    //console.log("URL is: " + URL);  
+    
+    
+    }
+
+
+
+const Layout = () => {
+  
+//  let test1 = lr.whtml(titleBanner01,1000,titleBanner02);
+//  console.log("test is: " + test1);
+  
+  
+ 
+  
+  
+  
+  return (
+    <div>     
+        
+      <div style={{textAlign:'center'}}>
+        <img  src={lr.whtml(titleBanner01,1000,titleBanner02)} 
+              alt="LR Graphic Here"
+              style={{
+                      width:'98%',  
+                      marginTop:"1%",
+                      border:'2px solid white', 
+                      borderRadius:lr.w("em",0.75,0.45)
+                    }}>
+        </img>
+      </div><br />    
+
       
-          <div>
-          <Link to="/"><button>Welcome</button></Link>
+      <div style={{color:'white'}}>{}</div>  
+        
+        
+        
+      <div style={{textAlign:"center"}}> 
+        <div>
+        <Link to="/"><button className="btnHover" style={{backgroundColor:'yellow', padding:'0.2%', borderRadius:'0.3em'}}>Welcome</button></Link>
           &nbsp;&nbsp;&nbsp;
-          <Link to="/methods"><button>Methods</button></Link>
+          <Link to="/methods" onClick={() => update()}><button>Methods</button></Link>
+                                                    
+                                                    
+                                                    
           &nbsp;&nbsp;&nbsp;
           <Link to="/explained"><button>Explained</button></Link>
-          </div>
-            
-          
-        
-        
+          &nbsp;&nbsp;&nbsp;
+          <Link to="/about"><button>About</button></Link>
+          &nbsp;&nbsp;&nbsp;
+          <Link to="/default"><button>Default</button></Link> 
+          &nbsp;&nbsp;&nbsp;
+          <Link to="/starting"><button>Starting</button></Link>
+        </div>
+      </div>
+      <div style={{color:'white'}}>{window.location.href}</div>
       <Outlet />
     </div>
   );
--- lrreact/src/lr-pages/Welcome.js
+++ lrreact/src/lr-pages/Welcome.js
@@ -1,5 +1,57 @@
+import React from 'react';
+
+import titleBanner01 from '../images/lr-title-banner_01.jpg';
+import titleBanner02 from '../images/lr-title-banner_02.jpg';
+import {lr} from '../lr/lr-single.js';
+
 function Welcome(){
-  return (<h1>This is the welcome page</h1>);
+  
+  return ( 
+          
+    <React.StrictMode>      
+      <div style={{textAlign:'center'}}>
+        <h1>This is the welcome page</h1>
+      </div>      
+
+      <div  className="" 
+            id="titling" 
+            style={{
+                    width: lr.w("%",55,98), 
+                    marginLeft:'auto', 
+                    marginRight:'auto', 
+                    backgroundColor:'#bbbbbb',
+                    border:'2px solid white', 
+                    borderRadius:'0.75em', 
+                    fontFamily: 'Arial Rounded MT Bold'
+                    /*fontFamily:'Tahoma'*/
+                  }}>
+        <div style={{textAlign:'center'}}>
+          <div className="arialRnd" style={{fontSize: lr.w("em",3.5,1.5), lineHeight:'150%', padding:'1% 0 0 0', textAlign:'center'}}>
+              Welcome to LR
+          </div>    
+          <ul style={{paddingLeft:'8%', textAlign:'left'}}>
+            <li style={{textAlign:'left'}}>
+              LR stands for Linear Response
+            </li>
+            <li style={{textAlign:'left'}}>
+              LR is an API for the responsive aspect of web design
+            </li>
+            <li style={{textAlign:'left'}}>
+              LR uses standard javascript
+            </li>
+            <li style={{textAlign:'left'}}>
+              LR works with all CSS and Javascript frameworks
+            </li>
+            <li style={{textAlign:'left'}}>
+              LR has powerful response tools
+            </li>
+          </ul>
+        </div>  
+      </div>  
+    </React.StrictMode>
+    
+    
+  );
 };
 
 export default Welcome;
